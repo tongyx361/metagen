a3f45dbac13aaa343bac13c658046aa15199e3e7
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-04-07 14:51:56 +0000

    fix: DP server

--- scripts/launch_dp_server.sh
+++ scripts/launch_dp_server.sh
@@ -7,18 +7,19 @@ MODEL_NAME=${MODEL_NAME:-${MODEL_PATH}}
 num_tot_gpus=$(nvidia-smi --query-gpu=count --format=csv,noheader,nounits | head -n 1)
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-$(seq -s, 0 $((num_tot_gpus - 1)))}
 TP_SIZE=${TP_SIZE:-1}
-IP=${IP:-"0.0.0.0"}
 BASE_PORT=${BASE_PORT:-8000} # For DP router
 DEBUG=${DEBUG:-0}
+IP=${IP:-"0.0.0.0"}
+SERVER_LOG_DIR=${SERVER_LOG_DIR:-"./logs/server"}
+ROUTER_LOG_DIR=${ROUTER_LOG_DIR:-"./logs/router"}
 
 read -a gpu_ids <<< "${CUDA_VISIBLE_DEVICES//,/ }"
-echo "gpu_ids=${gpu_ids[@]}"
 i_server=0
 num_gpus=${#gpu_ids[@]}
 worker_urls=()
 while [ $((i_server * TP_SIZE)) -lt ${num_gpus} ]; do
     port=$((BASE_PORT + i_server + 1))
-    worker_url="http://${IP}:${port}"
+    worker_url="http://localhost:${port}"
     worker_urls+=("${worker_url}")
     # [i_server * TP_SIZE, i_server * TP_SIZE + TP_SIZE) concatenated with commas
     read -a serve_gpu_ids <<< "${gpu_ids[@]:$((i_server * TP_SIZE)):${TP_SIZE}}" # e.g. 0 1
@@ -30,12 +31,14 @@ CUDA_VISIBLE_DEVICES=${serve_gpu_ids_str} \
 uvx vllm serve "${MODEL_PATH}" \
 --served-model-name "${MODEL_NAME}" \
 --tensor-parallel-size ${TP_SIZE} \
---host ${IP} --port ${port}
+--host localhost --port ${port} \
+> "${SERVER_LOG_DIR}/$(date +%Y%m%d-%H%M%S)-port${port}.log" 2>&1  &
 EOF
 
     if [ "${DEBUG}" -eq 1 ]; then
         echo "${serve_cmd}"
     else
+        mkdir -p "${SERVER_LOG_DIR}"
         eval "${serve_cmd}"
     fi
     i_server=$((i_server + 1))
@@ -45,11 +48,14 @@ read -r -d '' router_cmd << EOF
 uv run --with sglang-router \
     python -m sglang_router.launch_router \
     --worker-urls ${worker_urls[@]} \
-    --host "${IP}" --port "${BASE_PORT}"
+    --host "${IP}" --port "${BASE_PORT}" \
+    2>&1 | tee "${ROUTER_LOG_DIR}/$(date +%Y%m%d-%H%M%S)-port${BASE_PORT}.log"
 EOF
 
 if [ "${DEBUG}" -eq 1 ]; then
     echo "${router_cmd}"
 else
+    unset http_proxy
+    mkdir -p "${ROUTER_LOG_DIR}"
     eval "${router_cmd}"
 fi

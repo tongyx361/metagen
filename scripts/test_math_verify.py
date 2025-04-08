b8558f6d43de290ae73ef150b8251b635f0bfc35
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-04-08 14:37:45 +0000

    fix: verify config

--- scripts/test_math_verify.py
+++ scripts/test_math_verify.py
@@ -1,5 +1,8 @@
-from math_verify import parse  # type: ignore[import]
-from math_verify.parser import StringExtractionConfig  # type: ignore[import]
+from math_verify import parse, verify  # type: ignore[import]
+from math_verify.parser import (  # type: ignore[import]
+    LatexExtractionConfig,
+    StringExtractionConfig,
+)
 
 assert parse("\\boxed{A}", extraction_config=[StringExtractionConfig()]) == []
 assert parse("The answer is: A", extraction_config=[StringExtractionConfig()]) == [
@@ -9,3 +12,19 @@ assert parse("The answer is: A", extraction_config=[StringExtractionConfig()]) =
 assert parse(
     "A", extraction_config=[StringExtractionConfig(try_extract_without_anchor=True)]
 ) == ["a", "A"]
+
+assert verify(
+    gold=parse("A", extraction_config=[StringExtractionConfig()]),
+    target=parse(
+        "\\boxed{A}",
+        extraction_config=[LatexExtractionConfig(), StringExtractionConfig()],
+    ),
+)
+
+assert not verify(
+    gold=parse("A", extraction_config=[StringExtractionConfig()]),
+    target=parse(
+        "apple",
+        extraction_config=[LatexExtractionConfig(), StringExtractionConfig()],
+    ),
+)
